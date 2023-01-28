243137c1d08d83716e2da0c749fe4f6718ac46ed
alice_lbh <103922962+alicelbh@users.noreply.github.com>
2023-01-28 18:15:49 +0100

    Adding check for mq existence

--- house.py
+++ house.py
@@ -4,7 +4,7 @@ from threading import Thread, Lock
 import array
 import time
 import sysv_ipc
-import sys
+import sys, os
 import socket
 
 def produceEnergy(pR, s, n, mutex):
@@ -15,6 +15,9 @@ def produceEnergy(pR, s, n, mutex):
             if s[0]>0:
                 s[0]+=1
                 print("Energy stock : ", s[0])
+            else:
+                print("The house has reached its energy limit and is not functionning until it received energy.")
+                
        # print("Producing over")
             
 def consumeEnergy(cR, s, mutex):
@@ -25,10 +28,11 @@ def consumeEnergy(cR, s, mutex):
             if s[0]>0:
                 s[0]-=1
                 print("Energy stock : ", s[0])
-        #print("Consuming over")
+            else:
+                print("The house has reached its energy limit and is not functionning until it received energy.")
 
 def energyTrade(n, s, mutex, c, p, msg_queue, k, pol, host, port):
-    while(True):
+    while(checkQueueExistence(128)):
         time.sleep(1)
         with mutex:
             if s[0]>n and c>p:
@@ -38,6 +42,7 @@ def energyTrade(n, s, mutex, c, p, msg_queue, k, pol, host, port):
             if s[0]<n and p>c:
                 print(".....Buying.....")
                 buy(n, s, msg_queue, k, host, port)
+
 
 def sell(n, s, msg_queue, pol, host, port):
     buyerInterested = False
@@ -61,14 +66,17 @@ def sell(n, s, msg_queue, pol, host, port):
             try:
                 new_mq = sysv_ipc.MessageQueue(newKey)
                 print("      Entered channel ", newKey)
-                energySend = str(s[0]-n) #we give the amount of energy we have in excess
-                new_mq.send(energySend.encode(),type=newKey)     
-                s[0] = n
-                print("         Post-trade stock : ", s[0], ".....")  
+                if new_mq.current_messages==0:
+                    energySend = str(s[0]-n) #we give the amount of energy we have in excess
+                    new_mq.send(energySend.encode(),type=newKey)     
+                    s[0] = n
+                    print("         Post-trade stock : ", s[0], ".....")
+                else:
+                    print("     Someone has already sent electricity. Going back to main channel")  
             except:
                 #print(sys.exc_info()[0])
                 print("      Buyer has retracted. Going back to main channel")
-                sell(n, s, msg_queue)
+                sell(n, s, msg_queue, pol, host, port)
 
 
 def buy(n, s, msg_queue, k, host, port):
@@ -77,26 +85,29 @@ def buy(n, s, msg_queue, k, host, port):
         msg_queue.send(msg, type=1)
         print("   Sent buying offer")
         new_mq = sysv_ipc.MessageQueue(k, sysv_ipc.IPC_CREX)
-        time.sleep(1)
+        time.sleep(2)
         m, t = new_mq.receive(False)
         print("      I received ", m.decode())
         s[0]+= int(m.decode()) 
         print("         Post-trade energy stock : ", s[0])  
         new_mq.remove()    
     except:
-        e = sys.exc_info()[0]
         print("      No seller. Going to market")
         #print(e)
-        new_mq.remove()
+        if(checkQueueExistence(k)):
+            new_mq.remove()
         market(host, port, n, s)
-    time.sleep(3)
+
 
 def market(host, port, n, s):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((host, port))
+        try: 
+            client_socket.connect((host, port))
+        except: 
+            print("   Connection to market refused.")
+            return 0
         print("   Trading " + str(n-s[0]))
         data = str(n-s[0])
-
         client_socket.send(data.encode())
         resp = client_socket.recv(1024)
         if not len(resp):
@@ -107,19 +118,31 @@ def market(host, port, n, s):
         print("   Post-trade stock: " + str(s[0]))
 
 
+def checkQueueExistence(key):
+    try:
+        test = sysv_ipc.MessageQueue(key)
+        return True
+    except:
+        print(sys.exc_info()[0])
+        return False
+        
+
+def checkMainQueue():
+    while(True):
+        time.sleep(1)
+        try:
+            test = sysv_ipc.MessageQueue(128)
+        except:
+            print("Simulation closed")
+            os._exit(1)
+
+
 if __name__ == "__main__":
-    if sys.argv[5]== "y":
-        try:
-            mq = sysv_ipc.MessageQueue(128, sysv_ipc.IPC_CREX)
-        except:
-            print("Message queue", 128, "already exsits, terminating.")
-            sys.exit(1)
-    else:
-        try:
-            mq = sysv_ipc.MessageQueue(128)
-        except:
-            print("Cannot connect to message queue", 128, ", terminating.")
-            sys.exit(1) 
+    try:
+        mq = sysv_ipc.MessageQueue(128)
+    except:
+        print("Cannot connect to message queue", 128, ", terminating.")
+        sys.exit(1) 
 
     HOST = "localhost"
     PORT = 1790 
@@ -138,10 +161,13 @@ if __name__ == "__main__":
     production = Thread(target=produceEnergy, args=(initialProdRate,energyStock,energyNeeds, lock,))
     consumption = Thread(target=consumeEnergy, args=(initialConsRate,energyStock,lock,))
     trade = Thread(target=energyTrade, args=(energyNeeds, energyStock, lock, initialConsRate, initialProdRate, mq, key, policy, HOST, PORT))
+    check = Thread(target=checkMainQueue)
 
     production.start()
     consumption.start()
     trade.start()
+    check.start()
     production.join()
     consumption.join()
     trade.join()
+    check.join()
